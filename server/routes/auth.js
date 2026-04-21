d2736065c4dde11f98c879e5c9d221c0caac0284
mohit-git1 <babafrusty2826@gmail.com>
2026-04-21 13:48:09 +0530

    mongodb setup and auth routes and user settings configured

---
+++ server/routes/auth.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/signup", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+  const user = await User.create({
+    email: req.body.email,
+    password: hashed
+  });
+  res.json(user);
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("User not found");
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.status(400).send("Wrong password");
+
+  const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET);
+  res.json({ token });
+});
+
+module.exports = router;
